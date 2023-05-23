9a97e53fe691d011aa8c683b747858a89e22092a
Wanton Raven <josephngalu96@gmail.com>
2023-05-23 11:55:29 +0000

    updated the app.js file

--- mikutano/src/App.js
+++ mikutano/src/App.js
@@ -1,22 +1,22 @@
-import logo from './logo.svg';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
 import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
+import 'firebase/analytics';
 
-import { useAuthState } from 'react=firebase-hooks/auth';
+import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
 firebase.initializeApp({
-  apiKey: "AIzaSyDBWlPvc8TSFsftAwSzSef7X7aLA-xfZRk",
-  authDomain: "mikutano-d2421.firebaseapp.com",
-  projectId: "mikutano-d2421",
-  storageBucket: "mikutano-d2421.appspot.com",
-  messagingSenderId: "602911919949",
-  appId: "1:602911919949:web:f0b481028d6eedaa037104",
-  measurementId: "G-CY3YN6WSCN"
+  // your config
 })
+
+const auth = firebase.auth();
+const firestore = firebase.firestore();
+const analytics = firebase.analytics();
+
 
 function App() {
 
@@ -25,11 +25,14 @@ function App() {
   return (
     <div className="App">
       <header>
-        
+        <h1>⚛️🔥💬</h1>
+        <SignOut />
       </header>
+
       <section>
-        {user ? <Chatroom /> : <SignIn /> }
+        {user ? <ChatRoom /> : <SignIn />}
       </section>
+
     </div>
   );
 }
@@ -40,52 +43,81 @@ function SignIn() {
     const provider = new firebase.auth.GoogleAuthProvider();
     auth.signInWithPopup(provider);
   }
-  }
-
-  return (
-    <button onClick={signInWithGoogle}>Sign in with Google</button>
-  )
-}
-
-function SignOut() {
-  return auth.currentuser && (
-
-   <button onClick={() => auth.signOut()}>Sign Out</button> 
-  )
-}
-
-function ChatRoom() {
-
-  const messageRef = firestore.collection('messages');
-  const query = messagesRef.orderBy('createdAt').limit(25);
-
-  const [messages] = useCollectionData(query, {idField: 'id'});
 
   return (
     <>
-      <div> 
-        {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg}/>)}
-      </div>
-
-      <form>
-        <button type="submit"></button>
-      </form>
+      <button className="sign-in" onClick={signInWithGoogle}>Sign in with Google</button>
+      <p>Do not violate the community guidelines or you will be banned for life!</p>
     </>
+  )
+
+}
+
+function SignOut() {
+  return auth.currentUser && (
+    <button className="sign-out" onClick={() => auth.signOut()}>Sign Out</button>
   )
 }
 
+
+function ChatRoom() {
+  const dummy = useRef();
+  const messagesRef = firestore.collection('messages');
+  const query = messagesRef.orderBy('createdAt').limit(25);
+
+  const [messages] = useCollectionData(query, { idField: 'id' });
+
+  const [formValue, setFormValue] = useState('');
+
+
+  const sendMessage = async (e) => {
+    e.preventDefault();
+
+    const { uid, photoURL } = auth.currentUser;
+
+    await messagesRef.add({
+      text: formValue,
+      createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+      uid,
+      photoURL
+    })
+
+    setFormValue('');
+    dummy.current.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  return (<>
+    <main>
+
+      {messages && messages.map(msg => <ChatMessage key={msg.id} message={msg} />)}
+
+      <span ref={dummy}></span>
+
+    </main>
+
+    <form onSubmit={sendMessage}>
+
+      <input value={formValue} onChange={(e) => setFormValue(e.target.value)} placeholder="say something nice" />
+
+      <button type="submit" disabled={!formValue}>🕊️</button>
+
+    </form>
+  </>)
+}
+
+
 function ChatMessage(props) {
-  const { text, uid } = props.message;
+  const { text, uid, photoURL } = props.message;
 
   const messageClass = uid === auth.currentUser.uid ? 'sent' : 'received';
 
-  return (
-  <div className={''}>
-    <img src={photoURL} />
-
-    <p>{text}</p> 
-  </div>
-  )
+  return (<>
+    <div className={`message ${messageClass}`}>
+      <img src={photoURL || 'https://api.adorable.io/avatars/23/abott@adorable.png'} />
+      <p>{text}</p>
+    </div>
+  </>)
 }
+
 
 export default App;
